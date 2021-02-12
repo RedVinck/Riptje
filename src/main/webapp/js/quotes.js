e27bee7300042cf11b0fc19b843952128910b336
Elke Steegmans <Elke.Steegmans@ucll.be>
2021-02-12 14:47:04 +0100

    Alternatieve code toegevoegd

--- src/main/webapp/js/quotes.js
+++ src/main/webapp/js/quotes.js
@@ -7,10 +7,20 @@ async function getQuotes() {
 	await getQuotes();
 }
 
+// alternatieve code die het zelfde doet
+/*
+function getQuotes() {
+	fetch("/ManageQuoteServlet?command=quotes").then(response => response.json()).then(quotes => showQuotes(quotes));
+}*/
+
 function showQuotes (quotes) {
 	let quoteDiv = document.getElementById("quotes");
 	quoteDiv.innerHTML= "";
 	for(let i=0; i<quotes.length; i++) {
 		quoteDiv.innerHTML += "<p>"+ quotes[i].text + " by " + quotes[i].author + "</p>";
 	}
+	// alternatieve code die hetzelfde doet
+	/*
+	setTimeout(getQuotes, 1000);
+	*/
 }
